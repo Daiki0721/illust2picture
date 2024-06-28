8698dc20d3dd35e4beb4e439e17550d03045b856
daiki <daiki139045@gmail.com>
2024-06-28 18:06:32 +0900

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,15 @@
 import logo from './logo.svg';
-import './App.css';
+import BoxSystemProps from './components/header.jsx';
+import InputFileUpload from './components/submit.jsx';
+import FixedContainer from './components/result.jsx';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+  <>
+     <BoxSystemProps/>
+     <InputFileUpload/>
+     <FixedContainer/>
+  </>
   );
 }
 
